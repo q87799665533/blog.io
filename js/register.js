1b6eac00326a6eab9a80f48386997450b98a5c0a
qiji <727849003@qq.com>
2019-10-30 17:03:08 +0800

    .

--- js/register.js
+++ js/register.js
@@ -22,7 +22,7 @@ define(function (require) {
   }
 
   function bindEvents() {
-    $(document).on('tap', '.immediatelyBtn', function () {
+    $(document).on('click', '.immediatelyBtn', function () {
       if (!(/^1[34578]\d{9}$/.test($('.mobile_input').val()))) {
         toast.show('手机号码格式不正确');
       } else if ($('.code_input').val() === '') {
@@ -34,10 +34,10 @@ define(function (require) {
         });
       }
     });
-    $(document).on('tap', '.copyBtn', function () {
+    $(document).on('click', '.copyBtn', function () {
       copyModal.close();
     });
-    $(document).on('tap', '.code_btn', function () {
+    $(document).on('click', '.code_btn', function () {
       if (!(/^1[34578]\d{9}$/.test($('.mobile_input').val()))) {
         toast.show('手机号码格式不正确');
       } else {
